5ef45e6f90f0eda4265f109e9930fbf4daf2b4e0
athar <iitropar109@gmail.com>
2025-12-11 11:19:06 +0530

    second commit

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -37,6 +37,7 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 export async function createBooking(bookingData, formData) {
+  console.log("BookingDatacreate:", bookingData);
   bookingData = {
     ...bookingData,
     startDate: new Date(`${bookingData.startDate}T00:00:00.000Z`),
